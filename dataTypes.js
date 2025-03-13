f3d034d43a8b190e3062b702e387af6280443812
Sareij <sareij1234@gmail.com>
2025-03-13 05:48:16 +0530

    day6 javascript started

---
+++ dataTypes.js
@@ -0,0 +1,45 @@
+//?primitive
+//?string
+//'' "" ``
+
+var a={}
+var b={}
+console.log(a==b);
+
+
+let name1='saranya'
+console.log(name1);
+console.log(typeof name);
+
+let age=18
+console.log(age);
+console.log(typeof age);
+
+let Ishobby=false
+console.log(Ishobby);
+console.log(typeof Ishobby);
+
+let bankBalance=null;
+console.log(bankBalance);
+console.log(typeof bankBalance);
+
+let sea=undefined
+console.log(sea);
+console.log(typeof sea);
+
+let a3b=20;
+let username="saranya";
+let $ = 20;
+console.log($);
+let _ = 30;
+console.log(_);
+
+let IF="yes"
+console.log(IF);
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+// console.log("this is external file")
+// console.log(2+2);
+
+
+
+var a;
+a=10;
+a=30;
+var a=10;
+var a=20;
+
+
+let b;
+b=10;
+b=20;
+// let b=30;
+
+
+// const c;
+// c=10;
+// c=20;
+const c=10;
+
