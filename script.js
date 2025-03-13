f8915a3681841a11eed08c839e0dc992ce510b19
Sareij <sareij1234@gmail.com>
2025-03-13 05:50:54 +0530

    day 7 :   javascript operators, functions

---
+++ script.js
@@ -0,0 +1,129 @@
+let a=40;
+let b=30;
+
+//Arithmetic operator?
+// console.log(a+b);
+// console.log(a+'10');
+
+// console.log(a-b);
+// console.log(a-'10');
+
+// console.log(a/b);
+
+// console.log(a%b);
+
+// // 3cases in %
+// console.log(13%3); //1
+// console.log(1%2); //1
+// console.log(2%2); //0
+
+// //comparison operator
+
+// console.log(2==2);
+// console.log(2===2);
+// console.log(2=='2');
+// console.log(2==='2');
+// console.log([]==[]);
+// console.log([]===[]);
+// console.log(true>true); //1>1
+// console.log(true>false); //1>0
+// console.log(false>true);  //0>1
+// console.log(false>false); //0>0
+// console.log(true<true); //1<1
+// console.log(true<false); //1<0
+// console.log(false<true);  //0<1
+// console.log(false<false); //0<0
+
+//logical operator
+
+// console.log((2>2)&&(2<2));
+// console.log((2>2)||(2<2));
+// console.log(!(2>2));
+
+// let age=prompt("Enter your age");
+// let isEligibleForVote;
+
+// isEligibleForVote=(age>=18 ? console.log("eligible"): console.log("Not eligible"))
+// console.log(isEligibleForVote);
+
+// let number=prompt("Enter your Number");
+// let evenorodd;
+// evenorodd=(number%2==0 ? "even" : "odd")
+// console.log(evenorodd);
+
+// let age=prompt("Enter your age");
+//  if(age>=18){
+//     console.log("eligible for vote");
+    
+//  }
+//  else{
+//     console.log("not eligible for vote");
+    
+//  }
+
+
+// let price=prompt("Enter your price")
+
+// if(price>=5000){
+//     let discount1=5000-1000;
+//     console.log("your total bill was 5000 after getting 20% is " +discount1);
+    
+// }
+// else if(3000<price<5000)
+//     {
+//    let discount2=5000-750;
+//    console.log("your total bill was 5000 after getting 15% is "+discount2);
+   
+   
+// }
+// else if (1000<price<3000){
+//     let discount3=5000-500;
+//    console.log("your total bill was 5000 after getting 15% is "+discount3);
+// }
+// else {
+//        let amount=600;
+//       let  discount4=5000-500;
+//        console.log("please avail offer if you buy for 1000 "+discount4 );
+       
+// }
+
+//while loop
+
+// let i=0;
+// while(i<=50){
+//     if(i%2==0){
+//         console.log("Even number:"+i);
+//         i=i+2;
+        
+//     }
+// }
+
+//?for 
+// var i = 0 ;
+
+// for(i;i<10;i++){
+//     console.log("Hellooo..");
+    
+// }
+
+// num=prompt("Enter a number ")
+
+// var i = 0;
+// for(i;i<num;i=i+2){
+//     console.log("Even number "+i);
+    
+// }
+
+// let num1 = 12;
+// let num2=parseInt(prompt("Enter number "))
+// console.log(typeof num2);
+// console.log(num1===num2);
+
+// function add(num1,num2){
+//     console.log("the addition of two numbers is " +(num1+num2));
+//     return num1+num2;
+
+    
+// }
+//  add(10,20);
+// console.log( add(10,20));
